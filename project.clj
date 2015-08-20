6a68ad7da0f96a78a83a4a7de54a48e99e32d5c3
Rafal Lewczuk <rafal.lewczuk@gmail.com>
2015-08-20 09:03:58 +0200

    Finish implementation of basic LDAP functions.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kanar-ldap "0.1.0-SNAPSHOT"
+(defproject kanar/kanar-ldap "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- src/kanar/ldap.clj
+++ src/kanar/ldap.clj
@@ -2,49 +2,95 @@
   "LDAP authentication and principal resolve."
   (:require
     [clj-ldap.client :as ldap]
-    [slingshot.slingshot :refer [throw+]]
-    [kanar.core :as kc]))
+    [slingshot.slingshot :refer [try+ throw+]]
+    [kanar.core.util :as ku]
+    [taoensso.timbre :as log]))
 
 
-(defn ldap-authenticator [ & {:keys [conn base query-str user-re] :or {:user-re #"\W+"}}]
-  (fn [_ {{username :username password :password} :params}]
-    (if (re-matches user-re username)
-      (let [entries (ldap/search conn base {:attributes [:dn] :filter (format query-str username)})]
-        (cond
-          (empty? entries)
-            (throw+ {:type :login-failed, :error "Invalid username or password."})
-          (not (empty? (next entries)))
-            (throw+ {:type :fatal-error, :error "Error in account database. Please report to administrator."})
-          :else
-          (let [dn (:dn (first entries))]
-            (if (ldap/bind? conn dn password)
-              ; TODO analyze various cases here: account blocked, password expired etc.
-              {:id username :dn dn :attributes {}}
-              (throw+ {:type :login-failed, :error "Invalid username or password."})))
-          ))
-      (throw+ {:type :login-failed :error "Invalid username or password."})
-      ; TODO log this to security log
-      )))
+(def edir-err-defs
+  [[#"NDS error.*197.*" {:type :login-failed :msg "Account locked."}]
+   [#"NDS error.*215.*" {:type :chpass-failed :msg "Password previously used."}]
+   [#"NDS error.*220.*", {:type :login-failed :msg "Account expired."}]
+   [#"NDS error: bad password ..222.", {:type :login-failed, :msg "Login failed."}]
+   [#"NDS error.*222.*" {:type :login-failed :msg "Password expired."}]
+   [#"NDS error: failed authentication ..669." :msg "Login failed."]
+   ;[#"NDS error.*669.*" {:type :chpass-failed :msg "Wrong password."}]
+   [#".*" {:type :login-failed :msg "Login failed." }]])
 
 
-(defn ldap-attr-resolver [ & {:keys [conn base attr-map]}]
-  (fn [{:keys [id dn]} _]
-    (let [entries (ldap/search conn base {:attributes (vec (keys attr-map))})]
+(def msad-err-defs
+  [[#".*error, data 530.*" {:type :login-failed :msg "Not permitted at this time."}]
+   [#".*error, data 532.*" {:type :login-failed :msg "Password expired."}]
+   [#".*error, data 533.*" {:type :login-failed :msg "Account disabled."}]
+   [#".*error, data 701.*" {:type :login-failed :msg "Account expired."}]
+   [#".*error, data 701.*" {:type :login-failed :msg "Password must be reset."}]
+   [#".*error, data 775.*" {:type :login-failed :msg "Account locked."}]
+   [#".*" {:type :login-failed :msg "Login failed." }]])
+
+
+(defn dispatch-error [err-defs e]
+  (doseq [[rex exc] err-defs]
+    (if (re-matches rex (.getMessage e))
+      (throw+ exc)))
+  (ku/login-failed "Login failed."))
+
+
+(defn ldap-bind [ldap-conf err-defs dn password]
+  (try+
+    (let [conn (ldap/connect (assoc ldap-conf :num-connections 1 :bind-dn dn :password password))]
+      (log/debug "Successfully bound as " dn)
+      (.close conn))
+    (catch Exception e
+      (log/warn "Error binding user account " e)
+      (dispatch-error err-defs e))))
+
+
+(defn ldap-lookup-dn [conn {:keys [base-dn user-query user-re] :or {:user-re #"\W+"}} id]
+  (if (re-matches user-re id)
+    (let [query (format user-query id)
+          entries (ldap/search conn base-dn {:attributes [:dn] :filter query})]
+      (log/debug "Query=" query "results=" entries)
       (cond
-        (empty? entries)
-          (throw+ {:type :fatal-error, :error "Cannot resolve user data."})
-        (not (empty? (next entries)))
-          (throw+ {:type :fatal-error, :error "Error in account database. Please report to administrator."})
-        ; TODO log this as error and send to admin)
-        :else
-        {:id id
-         :attrs
-             (let [entry (first entries)]
-               (into {} (for [[k1 k2] attr-map] {k2 (k1 entry)})))})
-      )))
+        (empty? entries) (ku/login-failed "Invalid username or password.")
+        (not (empty? (next entries))) (ku/fatal-error "Error in users database. Please contact administrator.")
+        :else (:dn (first entries))))
+    (ku/login-failed "Invalid user name.")))
 
-(defn ldap-group-resolver [ & {:keys [conn u-base g-base]}]
 
-  )
-; TODO recursive LDAP group resolver;
+(defn ldap-auth-fn [conn ldap-conf err-defs]
+  (fn [_ {{username :username password :password} :params}]
+    (let [dn (ldap-lookup-dn conn ldap-conf username)]
+      (log/info "Found user DN: " dn)
+      (ldap-bind ldap-conf err-defs dn password)
+      {:id username :dn dn :attributes {}})))
+
+
+(defn ldap-lookup-fn [conn ldap-conf]
+  (fn [{id :id :as princ} _]
+    (let [dn (ldap-lookup-dn conn ldap-conf id)]
+      (log/info "Found user DN: " dn)
+      (assoc princ :dn dn))))
+
+
+(defn ldap-attr-fn [conn attr-map]
+  (fn [{:keys [dn attributes] :as princ} _]
+    (let [entry (ldap/get conn dn (keys attr-map))]
+      (if entry
+        (assoc princ
+          :attributes
+          (into (or attributes {}) (for [[k1 k2] attr-map] {k2 (k1 entry)})))
+        (ku/login-failed "Cannot obtain user data.")))))
+
+
+(defn ldap-roles-fn [conn attr to-attr regex]
+  (fn [princ _]
+    (let [entry (ldap/get conn (:dn princ) [attr])]
+      (if entry
+        (assoc-in
+          princ [:attributes :roles]
+          (filterv not-empty
+                   (for [g (attr entry)] (second (re-find regex g)))))
+        (ku/login-failed "Cannot obtain user data.")))))
+
+; TODO recursive LDAP group resolver
 
